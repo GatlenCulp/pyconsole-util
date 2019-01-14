6d7f12726f9e5f742f3dd609978f6449b3b5391e
Hugernot <hugernotyt@gmail.com>
2019-01-13 23:41:23 -0500

    Finished documentation and setup

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 import setuptools
-from pyconsole_util import util
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="Console Utilities",
-    version="0.0.1",
+    name="pyconsole-util",
+    version="1.0.0",
+
     author="Gatlen Culp",
     author_email="gatlenculp@gmail.com",
-    description="A set of functions that print to the console in different formats.",
+    description="A set of functions that print to the console in different formats for debugging, exploring, or printing nicely",
+    keywords="console printing colors formatting format debug info explorer",
+    license="GPLv3",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="github something",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    url="https://github.com/Hugernot/pyconsole-util",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Debuggers"
+    ]
 )
